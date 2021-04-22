c46f9bdbf244502054c99003e30b807cd5f99c64
mjaehaer <mjaehaer@users.noreply.github.com>
2021-04-23 01:40:52 +0300

    some

--- main.go
+++ main.go
@@ -1,15 +1,19 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
+	"os"
 	"strings"
 )
 
-func hello(w http.ResponseWriter, r *http.Request) {
+// type roots []string
+
+func process(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/metrics" {
 		http.Error(w, "404 not found.", http.StatusNotFound)
 		return
@@ -17,21 +21,39 @@ func hello(w http.ResponseWriter, r *http.Request) {
 
 	switch r.Method {
 	case "POST":
+		// var roots roots
+		var root string
 		var buf bytes.Buffer
 		file, header, err := r.FormFile("file")
 		if err != nil {
 			panic(err)
 		}
 		defer file.Close()
-		name := strings.Split(header.Filename, ".")
-		fmt.Printf("File name %s\n", name)
+		tempOutFile, _ := os.Open("tempOutFile.txt")
+		defer tempOutFile.Close()
+		fmt.Printf("File name %s\n", header.Filename)
 		io.Copy(&buf, file)
+		fmt.Printf("%s\n", &buf)
+		scanner := bufio.NewScanner(&buf)
+		for scanner.Scan() {
+			var strbuf string
+			if s1 := scanner.Text()[0]; s1 != 32 {
+				root = scanner.Text()[:len(scanner.Text())-1]
+			}
+			if s2 := scanner.Text()[1]; s2 == 45 {
 
-		contents := buf.String()
-		fmt.Println(contents)
-
+				a := strings.Split(scanner.Text(), ":")[0]
+				strbuf += root + "{" + strings.Split(a, "- ")[1] + "=" + "\"" + strings.Split(scanner.Text(), ": ")[1] + "\"" + "}"
+				fmt.Println(strbuf)
+			} else {
+				if s3 := scanner.Text()[0]; s3 == 32 {
+					strbuf += " " + strings.Split(scanner.Text(), ": ")[1]
+					println(strbuf)
+				}
+			}
+		}
+		// fmt.Println(root)
 		buf.Reset()
-
 		return
 	default:
 		fmt.Fprintf(w, "Sorry, only POST methods are supported.")
@@ -39,7 +61,7 @@ func hello(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/metrics", hello)
+	http.HandleFunc("/metrics", process)
 
 	fmt.Printf("Starting server for testing HTTP POST...\n")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
