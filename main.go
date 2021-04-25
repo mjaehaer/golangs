edf5ba2d6042713e77fb792ebdf0ceeb14f3a4fc
mjaehaer <mjaehaer@users.noreply.github.com>
2021-04-25 03:17:31 +0300

    commit

--- main.go
+++ main.go
@@ -5,13 +5,11 @@ import (
 	"bytes"
 	"fmt"
 	"io"
-	"log"
+	"io/ioutil"
 	"net/http"
 	"os"
 	"strings"
 )
-
-// type roots []string
 
 func process(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/metrics" {
@@ -21,7 +19,6 @@ func process(w http.ResponseWriter, r *http.Request) {
 
 	switch r.Method {
 	case "POST":
-		// var roots roots
 		var root string
 		var buf bytes.Buffer
 		file, header, err := r.FormFile("file")
@@ -29,14 +26,15 @@ func process(w http.ResponseWriter, r *http.Request) {
 			panic(err)
 		}
 		defer file.Close()
-		tempOutFile, _ := os.Open("tempOutFile.txt")
-		defer tempOutFile.Close()
+		tempOutFile, _ := os.Create("tempOutFile.openmetrics")
+		// defer tempOutFile.Close()
 		fmt.Printf("File name %s\n", header.Filename)
 		io.Copy(&buf, file)
 		fmt.Printf("%s\n", &buf)
 		scanner := bufio.NewScanner(&buf)
+		var strbuf string
 		for scanner.Scan() {
-			var strbuf string
+
 			if s1 := scanner.Text()[0]; s1 != 32 {
 				root = scanner.Text()[:len(scanner.Text())-1]
 			}
@@ -44,19 +42,33 @@ func process(w http.ResponseWriter, r *http.Request) {
 
 				a := strings.Split(scanner.Text(), ":")[0]
 				strbuf += root + "{" + strings.Split(a, "- ")[1] + "=" + "\"" + strings.Split(scanner.Text(), ": ")[1] + "\"" + "}"
-				fmt.Println(strbuf)
-			} else {
-				if s3 := scanner.Text()[0]; s3 == 32 {
-					strbuf += " " + strings.Split(scanner.Text(), ": ")[1]
-					println(strbuf)
+			}
+			if s3, s4 := scanner.Text()[0], scanner.Text()[1]; s3 == 32 && s4 != 45 {
+				strbuf += " " + strings.Split(scanner.Text(), ": ")[1] + "\n"
+				println(strbuf)
+				if _, err := tempOutFile.Write([]byte(strbuf)); err != nil {
+					fmt.Println(err)
 				}
+				strbuf = ""
 			}
 		}
-		// fmt.Println(root)
+
+		tempOutFile.Close()
+		renderFile(w, "tempOutFile.openmetrics")
 		buf.Reset()
 		return
 	default:
 		fmt.Fprintf(w, "Sorry, only POST methods are supported.")
+	}
+}
+
+func renderFile(w http.ResponseWriter, filename string) {
+	fmt.Println("Read request: " + filename)
+	file, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Cann't open file: " + filename)
+	} else {
+		w.Write(file)
 	}
 }
 
@@ -65,6 +77,6 @@ func main() {
 
 	fmt.Printf("Starting server for testing HTTP POST...\n")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
-		log.Fatal(err)
+		fmt.Println(err)
 	}
 }
